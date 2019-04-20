6ecf0815593f91ec20c6321bdff3ee15a7091a2b
SANDHIYA11 <49223761+SANDHIYA11@users.noreply.github.com>
2019-04-20 13:22:20 +0530

    Maxmin.py

---
+++ Maxmin.py
@@ -0,0 +1,8 @@
+L=[]
+N=int(input("Enter the how many number:")) 
+for i in range (N): 
+Num=int(input("Enter the number:")) 
+L.append(Num) 
+print("The maximum number is:", max(L)) 
+print("The minimum number is:", min(L))
+ 
