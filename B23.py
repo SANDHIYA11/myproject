97bc4845a2079dd87d7d3cd4b8251dfacbf693a1
SANDHIYA11 <49223761+SANDHIYA11@users.noreply.github.com>
2019-05-08 05:16:45 +0530

    B23.py

---
+++ B23.py
@@ -0,0 +1,7 @@
+from array import *
+x=array('i',[]) 
+a=int(input("Enter the number:")) 
+for i in range(a): 
+   n=int(input("Enter the numbers:")) 
+   x.append(n) 
+print("The minimum number is:", min(x)) 
